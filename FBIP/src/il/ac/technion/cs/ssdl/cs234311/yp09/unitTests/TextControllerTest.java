e7032a56b392602c40038c5949fa1910cfb25500
owais-musa <owais.musa@gmail.com>
2013-11-11 16:37:54 +0200

    add unitTest package

---
+++ FBIP/src/il/ac/technion/cs/ssdl/cs234311/yp09/unitTests/TextControllerTest.java
@@ -0,0 +1,9 @@
+package il.ac.technion.cs.ssdl.cs234311.yp09.unitTests;
+
+import junit.framework.TestCase;
+
+public class TextControllerTest extends TestCase {
+	public void testInsertChar() throws Exception {
+		fail();
+	}
+}
