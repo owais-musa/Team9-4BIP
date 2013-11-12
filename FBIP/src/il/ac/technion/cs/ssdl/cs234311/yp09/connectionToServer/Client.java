27b0aff4d206ba32df208e524f4cb60767588461
Tal Schwartz <talsch22@gmail.com>
2013-11-12 22:59:40 +0200

    different client
    the errors now contain string describing the error

--- FBIP/src/il/ac/technion/cs/ssdl/cs234311/yp09/connectionToServer/Client.java
+++ FBIP/src/il/ac/technion/cs/ssdl/cs234311/yp09/connectionToServer/Client.java
@@ -1,103 +1,153 @@
 package il.ac.technion.cs.ssdl.cs234311.yp09.connectionToServer;
 
 import java.lang.String;
-
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.InputStreamReader;
-//import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
-import java.net.ProtocolException;
 import java.net.URL;
+
  
-//import javax.net.ssl.HttpsURLConnection;
 
 public class Client implements ClientCom  {
+	
+	/*   -----------   ------------------  ------------------- **
+	**   -----------       CONSTUNTS       ------------------- **
+	**   -----------   ------------------  ------------------- */
 		
-	// for regular use
-	public ErrorType  send2Server(String message2send){ return  sendPost(message2send, false);}
+	private static final String ERROR_URL = "an ERROR acurred while creating URL";
+	private static final String ERROR_CONNECT = "an ERROR acurred while connecting to server" ;
+	private static final String ERROR_SET_POST = "problem setting POST";
+	private static final String ERROR_BAD_RESPONSE_CODE = " got a bad response code from server : ";
+	private static final String ERROR_OPEN_CONNECT = "problem opening output stream";
+	private static final String ERROR_OPEN_STREAM = " an ERROR while opening output stream";
+	private static final String ERROR_WRITING_MESSAGE ="error ecured while writing message";
+	private static final String ERROR_READING_RESPONSE = "could not read response";
+	private static final String ERROR_CLOSING_OUTPUTDATASTREAM = "could not close DataOutputStream";
+	private static final String URL_ADRRESS_DEFAULT = "http://1.talserver22.appspot.com/talserver";
 	
-	//for debug use
-	public ErrorType  send2ServerDebeg(String message2send){ return  sendPost(message2send, true);}
+	
+	/*   -----------   ------------------  ------------------- **
+	**   -----------       FIELDS          ------------------- **
+	**   -----------   ------------------  ------------------- */
+	private String URL_ADRRESS;
+	
+	
+	/*   -----------   ------------------  ------------------- **
+	**   -----------      CONSTRUCTORS     ------------------- **
+	**   -----------   ------------------  ------------------- */
+	public Client(){
+		URL_ADRRESS = URL_ADRRESS_DEFAULT;
+	}
+	
+	public Client(String url){
+		URL_ADRRESS = url;
+	}
+	
+	
+	/*   -----------   ------------------  ------------------- **
+	**   -----------     USER FUNCTIONS    ------------------- **
+	**   -----------   ------------------  ------------------- */
 
+	@Override
+	public ErrorType  send2Server(String message2send)		{ return  sendPost(message2send, false);}
 	
+	
+	/*   -----------   ------------------  ------------------- **
+	**   -----------     DEBUG FUNCTIONS   ------------------- **
+	**   -----------   ------------------  ------------------- */
+	
+	public ErrorType  send2ServerDebeg(String message2send){ return  sendPost(message2send, true);}
+	
+	
+	/*   -----------   ------------------  ------------------- **
+	**   -----------     COMM FUNCTIONS    ------------------- **
+	**   -----------   ------------------  ------------------- */
 	private ErrorType  sendPost(String message2send, boolean debugMode) {// sends a message to server
 		
-		String url = "http://1.talserver22.appspot.com/talserver";
 		URL obj =null;
 		
 		// make a new URL object out of address string
-		try {obj = new URL(url);
+		try {	obj = new URL(URL_ADRRESS);
 			}catch (MalformedURLException e) {
-			if (debugMode) System.out.println("an ERROR acurred while creating URL");// for debugging purpose
-			return ErrorType.SEND_FAIL;
+			return endWithError(ErrorType.SEND_FAIL, ERROR_URL, debugMode );
 			}
 		
 		HttpURLConnection con =null;
 		// open connection 
 		try {con = (HttpURLConnection) obj.openConnection();
 		} catch (IOException e) {
-			if (debugMode) System.out.println("an ERROR acurred while connecting to server");// for debugging purpose
-			return ErrorType.SEND_FAIL;
+			return endWithError(ErrorType.SEND_FAIL, ERROR_CONNECT, debugMode );
 			}
 		
 		// post message
 		try { con.setRequestMethod("POST");
 		}  catch (IOException e1) {
-			System.out.println("problem setting GET"); 
-			return ErrorType.SEND_FAIL;
+			return endWithError(ErrorType.SEND_FAIL, ERROR_SET_POST, debugMode );
 			}
 		
-		 
 		con.setDoOutput(true);
 		DataOutputStream wr = null;
-		try {
-			 wr = new DataOutputStream(con.getOutputStream());
-		} catch (IOException e1) {System.out.println("problem opening output stream");}
 		
-		
+		try { wr = new DataOutputStream(con.getOutputStream());
+		} catch (IOException e1) {
+			return endWithError(ErrorType.SEND_FAIL, ERROR_OPEN_STREAM, debugMode );
+		}
+				
 		try {
 			wr.writeBytes(message2send);
-		}  catch (IOException e1) {System.out.println("writing message");}
+		}  catch (IOException e1) {
+			return endWithError(ErrorType.SEND_FAIL, ERROR_WRITING_MESSAGE, debugMode );
+		}
 		 
 		int responseCode = -1;
 		// sending the GET message
 		try {responseCode = con.getResponseCode();
 		} catch (IOException e) {
-			if (debugMode) System.out.println("an ERROR acurred while tring to get response from server");// for debugging purpose
-			return ErrorType.SEND_FAIL;
+			return endWithError(ErrorType.SEND_FAIL, ERROR_OPEN_CONNECT, debugMode );
 			}
-		System.out.println("response code is :" + responseCode);
+
+		if (responseCode != 200 ) {// problem with sending the message
+			return endWithError(ErrorType.BAD_RESPONSE, ERROR_BAD_RESPONSE_CODE, debugMode );
+			}
 		
 		BufferedReader in =null;
 		try {
 			in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 		} catch (IOException e) {
-			if (debugMode) System.out.println("could not read response");}
+			return endWithError(ErrorType.BAD_RESPONSE, ERROR_READING_RESPONSE, debugMode );
+
+			}
 		
-		//  ONLY FOR DEBUG PURPOSES 
-		 if (debugMode){/// if we are in debug mode we would like to read the server response
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-	 
-			try {
-				while ((inputLine = in.readLine()) != null)	response.append(inputLine);
-			} catch (IOException e) { System.out.println("could not read response");		}
+		StringBuffer response = new StringBuffer();
+		String inputLine;
+
+		try {
+			while ((inputLine = in.readLine()) != null)	response.append(inputLine);
+		} catch (IOException e) { 
+			return endWithError(ErrorType.BAD_RESPONSE, ERROR_READING_RESPONSE, debugMode );
+		}
+		
+		try {	in.close();
+		}catch (IOException e){
+				return endWithError(ErrorType.BAD_RESPONSE, ERROR_CLOSING_OUTPUTDATASTREAM, debugMode );
+		}
 			
-			try {
-				in.close();
-			} catch (IOException e){ System.out.println("could not close DataOutputStream");}
-			
+			//  ONLY FOR DEBUG PURPOSES 
+		if (debugMode){/// if we are in debug mode we would like to read the server response
 			System.out.println("the response is : ");
 			System.out.println(response.toString());
 			}
 		 
-		 return ErrorType.NO_ERROR;
+		return endWithError(ErrorType.NO_ERROR, response.toString(), debugMode );
 	}
 	
-	
-
+	private static ErrorType endWithError (ErrorType et ,String errorMessage, boolean debugMode ){
+		
+		if (debugMode) System.out.println(errorMessage);
+		et.s = errorMessage;
+		return et;
+	}
 }
--- FBIP/src/il/ac/technion/cs/ssdl/cs234311/yp09/connectionToServer/ErrorType.java
+++ FBIP/src/il/ac/technion/cs/ssdl/cs234311/yp09/connectionToServer/ErrorType.java
@@ -1,7 +1,9 @@
 package il.ac.technion.cs.ssdl.cs234311.yp09.connectionToServer;
 
 public enum ErrorType { 
-	NO_ERROR ,SEND_FAIL, BAD_RESPONSE
-} 
+	NO_ERROR ,SEND_FAIL, BAD_RESPONSE;
+	public  String s;
+	ErrorType(){s="";}
+	 } 
 
 
